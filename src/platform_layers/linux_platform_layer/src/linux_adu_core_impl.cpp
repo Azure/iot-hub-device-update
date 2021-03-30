bbcfb1cc72d8117ec8478209309f6fa1946b82bc
Nicholas Hemstreet <62158276+nihemstr@users.noreply.github.com>
2021-03-30 14:57:30 -0700

    Added generic error handlers to the linux_adu_core_impl (#28)
    * Added generic error handlers to the linux_adu_core_impl
    
    * Added more interpretive use of the ADU_ERC_NOTRECOVERABLE

--- src/platform_layers/linux_platform_layer/src/linux_adu_core_impl.cpp
+++ src/platform_layers/linux_platform_layer/src/linux_adu_core_impl.cpp
@@ -186,6 +186,35 @@ ADUC_Result LinuxPlatformLayer::Download(const char* workflowId, const char* upd
 
         extendedResultCode = MAKE_ADUC_DELIVERY_OPTIMIZATION_EXTENDEDRESULTCODE(doErrorCode);
     }
+    catch (const std::exception& e)
+    {
+        Log_Error("DO download failed with an unhandled std exception: %s", e.what());
+
+        resultCode = ADUC_DownloadResult_Failure;
+        if (errno != 0)
+        {
+            extendedResultCode = MAKE_ADUC_ERRNO_EXTENDEDRESULTCODE(errno);
+        }
+        else
+        {
+            extendedResultCode = ADUC_ERC_NOTRECOVERABLE;
+        }
+    }
+    catch (...)
+    {
+        Log_Error("DO download failed due to an unknown exception");
+
+        resultCode = ADUC_DownloadResult_Failure;
+
+        if (errno != 0)
+        {
+            extendedResultCode = MAKE_ADUC_ERRNO_EXTENDEDRESULTCODE(errno);
+        }
+        else
+        {
+            extendedResultCode = ADUC_ERC_NOTRECOVERABLE;
+        }
+    }
 
     // If we downloaded successfully, validate the file hash.
     if (resultCode == ADUC_DownloadResult_Success)
