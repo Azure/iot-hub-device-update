1a6026716a454d78d7c197253b36901bfc1e5aa1
jw-msft <84477130+jw-msft@users.noreply.github.com>
2022-06-03 16:13:51 -0700

    Make osname deviceinfo more granular (#527)
    * Put name in osName and release in swVersion from os-release, lsb-release, uname
    * Add RemoveSurrounding string_utils UT

--- src/utils/string_utils/inc/aduc/string_utils.hpp
+++ src/utils/string_utils/inc/aduc/string_utils.hpp
@@ -39,6 +39,24 @@ static std::string& Trim(std::string& s)
     return s;
 }
 
+/**
+ * @brief Removes a char from the front and back of the string.
+ * @param s The string
+ * @param c The char to remove from front and back.
+ * @return std::string& The reference to the possibly mutated input string.
+ * @details If the string has the char at front and back, then input string will be mutated; else, it will be left untouched.
+ */
+static std::string& RemoveSurrounding(std::string& s, char c) // NOLINT(google-runtime-references)
+{
+    if (s.front() == c && s.back() == c)
+    {
+        s.erase(0, 1);
+        s.pop_back();
+    }
+
+    return s;
+}
+
 static std::vector<std::string> Split(const std::string& str, const char separator)
 {
     std::vector<std::string> tokens;
