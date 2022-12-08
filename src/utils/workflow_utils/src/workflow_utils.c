2ab17940e848f9050b5d78674b946a2e96df37af
jw-msft <84477130+jw-msft@users.noreply.github.com>
2022-12-07 21:42:28 -0800

    Fix leak of hash array in workflow_get_update_file

--- src/utils/workflow_utils/src/workflow_utils.c
+++ src/utils/workflow_utils/src/workflow_utils.c
@@ -1814,7 +1814,8 @@ bool workflow_get_update_file(ADUC_WorkflowHandle handle, size_t index, ADUC_Fil
         goto done;
     }
 
-    // transfer tempHash as it has been assigned into newEntity structure now
+    // ADUC_FileEntity_Init makes a deep copy of the hash so must free tempHash to avoid memory leak.
+    ADUC_Hash_FreeArray(tempHashCount, tempHash);
     tempHash = NULL;
 
     if (!ParseFileEntityDownloadHandler(handle, file, newEntity))
