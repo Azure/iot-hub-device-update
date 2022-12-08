c8c5b30a72e95cadeac908438649fd235c6f5bd5
jw-msft <84477130+jw-msft@users.noreply.github.com>
2022-12-07 21:52:53 -0800

    Fix many memleaks (valgrind) in MoveToUpdateCache

--- src/extensions/download_handlers/plugin_examples/microsoft_delta_download_handler/source_update_cache/src/source_update_cache_utils.c
+++ src/extensions/download_handlers/plugin_examples/microsoft_delta_download_handler/source_update_cache/src/source_update_cache_utils.c
@@ -158,9 +158,6 @@ ADUC_Result ADUC_SourceUpdateCacheUtils_MoveToUpdateCache(
     size_t countPayloads = workflow_get_update_files_count(workflowHandle);
     for (size_t index = 0; index < countPayloads; ++index)
     {
-        workflow_free_file_entity(fileEntity);
-        fileEntity = NULL;
-
         if (!workflow_get_update_file(workflowHandle, index, &fileEntity))
         {
             Log_Error("get update file %d", index);
@@ -220,8 +217,7 @@ ADUC_Result ADUC_SourceUpdateCacheUtils_MoveToUpdateCache(
         // errno EXDEV would be common if copying across different mount points.
         // For any failure, it falls back to copy.
 
-        Log_Debug(
-            "moving '%s' -> '%s'", STRING_c_str(sandboxUpdatePayloadFile), STRING_c_str(updateCacheFilePath));
+        Log_Debug("moving '%s' -> '%s'", STRING_c_str(sandboxUpdatePayloadFile), STRING_c_str(updateCacheFilePath));
 
         res = rename(STRING_c_str(sandboxUpdatePayloadFile), STRING_c_str(updateCacheFilePath));
         if (res != 0)
@@ -240,16 +236,26 @@ ADUC_Result ADUC_SourceUpdateCacheUtils_MoveToUpdateCache(
             }
         }
 
+        workflow_free_file_entity(fileEntity);
+        fileEntity = NULL;
+
+        ADUC_UpdateId_UninitAndFree(updateId);
+        updateId = NULL;
+
         STRING_delete(updateCacheFilePath);
         updateCacheFilePath = NULL;
+
+        STRING_delete(sandboxUpdatePayloadFile);
+        sandboxUpdatePayloadFile = NULL;
     }
 
     result.ResultCode = ADUC_Result_Success;
 
 done:
+    workflow_free_file_entity(fileEntity);
+    ADUC_UpdateId_UninitAndFree(updateId);
     STRING_delete(sandboxUpdatePayloadFile);
     STRING_delete(updateCacheFilePath);
-    workflow_free_file_entity(fileEntity);
 
     return result;
 }
