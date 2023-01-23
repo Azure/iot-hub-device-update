ece923349867d66bc48ecc07e60e4b3b7f2167e7
Jeff Miller <jeffmill@microsoft.com>
2023-01-23 14:56:11 -0800

    Make build.ps1 use PowerShell argument format; Add arguments parameter to debug_agent.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,40 @@
-# e.g.
-# scripts\build.ps1 --build-unit-tests --type RelWithDebInfo --platform-layer simulator --log-dir '/tmp/aduc-logs'
-# TODO(JeffMill): Change arg parsing to standard PowerShell Param()
-#
-# TODO(JeffMill): Scenario 2:--static-analysis clang-tidy,cppcheck --build-docs
+<#
+.SYNOPSIS
+Build the Device Update product.
+
+.DESCRIPTION
+Does all of the work to build the device update product, and provides additional support,
+such as static checks and building documentation.
+
+.EXAMPLE
+PS> ./Scripts/Build.ps1 -Type Debug -BuildUnitTests -Install -Clean
+#>
+Param(
+    # Deletes targets before building.
+    [switch]$Clean,
+    # Build type, e.g. Release, RelWithDebInfo, Debug.
+    [string]$Type = 'Debug',
+    # Should documentation be built?
+    [switch]$BuildDocumentation,
+    # Should unit tests be built?
+    [switch]$BuildUnitTests,
+    # Should deployment package be built?
+    [switch]$BuildPackages,
+    # Output directory. Default is {git_root}/out
+    [string]$OutputDirectory = "$(git.exe rev-parse --show-toplevel)/out",
+    # Static analysis tools to run, e.g. 'clang-tidy', 'cppcheck', 'cpplint', 'iwyu', 'lwyu'
+    [string[]]$StaticAnalysisTools,
+    # Logging library to use
+    [string]$LogLib = 'zlog',
+    # Log folder to use for DU logs
+    # TODO(JeffMill): Change this when folder structure determined.
+    [string]$LogDir = '/var/log/adu',
+    # Install the product locally
+    [switch]$Install,
+    # Where to install DU binaries to
+    # TODO(JeffMill): Not working.  Active bug on this.
+    [string]$InstallPrefix = '/usr/local'
+)
 
 function Show-Warning {
     Param([Parameter(mandatory = $true, position = 0)][string]$Message)
@@ -98,74 +130,6 @@ function Show-LinkerErrors {
 
 $root_dir = git.exe rev-parse --show-toplevel
 
-# TODO(JeffMill): Unused variable
-# $script_dir = "$root_dir/scripts"
-
-$build_clean = $false
-$build_documentation = $false
-$build_packages = $false
-$platform_layer = 'linux'
-$trace_target_deps = $false
-
-# TODO(JeffMill): Unable to filter handlers at build time:
-# Task 42405545: [P1] No way to selectively build content downloaders should I remove ADUC_CONTENT_HANDLERS for now?
-# Task 42398328: [P1] add the --content-handlers option back.
-# $content_handlers = 'microsoft/swupdate,microsoft/apt,microsoft/simulator'
-
-$build_type = 'Debug'
-$adu_log_dir = ''
-$default_log_dir = '/var/log/adu'
-$output_directory = "$root_dir/out"
-$build_unittests = $false
-$static_analysis_tools = @()
-$log_lib = 'zlog'
-$install_prefix = '/usr/local'
-
-# TODO(JeffMill): unused variable
-# $install_adu = $false
-
-# $work_folder = '/tmp'
-
-# TODO(JeffMill): unused variable
-# $cmake_dir_path = "$work_folder/deviceupdate-cmake"
-
-function print_help {
-    @'
-Usage: build.ps1 [options...]
--c, --clean                           Does a clean build.
--t, --type <build_type>               The type of build to produce. Passed to CMAKE_BUILD_TYPE. Default is Debug.
-                                      Options: Release Debug RelWithDebInfo MinSizeRel
--d, --build-docs                      Builds the documentation.
--u, --build-unit-tests                Builds unit tests.
---build-packages                      Builds and packages the client in various package formats e.g debian.
--o, --out-dir <out_dir>               Sets the build output directory. Default is out.
--s, --static-analysis <tools...>      Runs static analysis as part of the build.
-                                      Tools is a comma delimited list of static analysis tools to run at build time.
-                                      Tools: clang-tidy cppcheck cpplint iwyu lwyu (or all)
-
--p, --platform-layer <layer>          Specify the platform layer to build/use. Default is linux.
-                                      Option: linux
-
---trace-target-deps                   Traces dependencies of CMake targets debug info.
-
---log-lib <log_lib>                   Specify the logging library to build/use. Default is zlog.
-                                      Options: zlog xlog
-
--l, --log-dir <log_dir>               Specify the directory where the ADU Agent will write logs.
-                                      Only valid for logging libraries that support file logging.
-
---install-prefix <prefix>             Install prefix to pass to CMake.
-
---install                             Install the following ADU components.
-                                          From source: deviceupdate-agent.service & adu-swupdate.sh.
-                                          From build output directory: AducIotAgent & adu-shell.
-
---cmake-path                          Override the cmake path such that CMake binary is at <cmake-path>/bin/cmake
-
--h, --help                            Show this help message.
-'@
-}
-
 function Invoke-CopyFile {
     Param(
         [Parameter(mandatory = $true)][string]$Source,
@@ -260,7 +224,7 @@ function Register-Components {
 
 function Install-DeliveryOptimization {
     Param(
-        [Parameter(Mandatory = $true)][string]$BuildType,
+        [Parameter(Mandatory = $true)][string]$Type,
         [Parameter(Mandatory = $true)][string]$Path,
         [Parameter(Mandatory = $true)][string]$Branch,
         [Parameter(Mandatory = $true)][string]$Commit)
@@ -278,9 +242,9 @@ function Install-DeliveryOptimization {
     # do_url=git@github.com:Microsoft/do-client.git
     $do_url = 'https://github.com/Microsoft/do-client.git'
 
-    git clone --recursive --single-branch --branch $Branch --depth 1 $do_url .
+    git.exe clone --recursive --single-branch --branch $Branch --depth 1 $do_url .
 
-    git checkout $Commit
+    git.exe checkout $Commit
 
     $build_dir = 'cmake'
 
@@ -297,9 +261,9 @@ function Install-DeliveryOptimization {
 
     # Bug 43044349: DO-client cmakefile not properly building correct type using cmake
     # -DCMAKE_BUILD_TYPE should ultimately be removed.
-    $CMAKE_OPTIONS = '-DDO_BUILD_TESTS:BOOL=OFF', '-DDO_INCLUDE_SDK=ON', "-DCMAKE_BUILD_TYPE=$BuildType"
+    $CMAKE_OPTIONS = '-DDO_BUILD_TESTS:BOOL=OFF', '-DDO_INCLUDE_SDK=ON', "-DCMAKE_BUILD_TYPE=$Type"
     cmake.exe -S . -B $build_dir @CMAKE_OPTIONS
-    cmake.exe --build $build_dir --config $BuildType --parallel
+    cmake.exe --build $build_dir --config $Type --parallel
 
     Pop-Location
     ''
@@ -314,15 +278,15 @@ function Install-Adu-Components {
 
     # cmake --install should place binaries, but that's not working correctly.
     # TODO: Task 42936258: --install-prefix not working properly
-    # & $cmake_bin --install $output_directory --config $build_type --verbose
+    # & $cmake_bin --install $OutputDirectory --config $Type --verbose
     # $ret_val = $LASTEXITCODE
     # if ($ret_val -ne 0) {
-        #     Write-Error "ERROR: CMake failed (Error $ret_val)"
-        #     exit $ret_val
-        # }
+    #     Write-Error "ERROR: CMake failed (Error $ret_val)"
+    #     exit $ret_val
+    # }
     # Workaround: Manually copy files...
 
-    $bin_path = "$runtime_dir/$build_type"
+    $bin_path = "$runtime_dir/$Type"
     # contoso_component_enumerator
     # curl_content_downloader
     # microsoft_apt_1
@@ -343,7 +307,7 @@ function Install-Adu-Components {
     Invoke-CopyFile  "$bin_path/libcrypto-1_1-x64.dll" /usr/lib/adu
     Invoke-CopyFile  "$bin_path/libcrypto-1_1-x64.dll" /usr/bin
 
-    if ($build_type -eq 'Debug') {
+    if ($Type -eq 'Debug') {
         $pthread_dll = 'pthreadVC3d.dll'
     }
     else {
@@ -411,7 +375,7 @@ function Install-Adu-Components {
     #
 
     if (-not (Test-Path -LiteralPath '/etc/adu/du-config.json' -PathType Leaf)) {
-@'
+        @'
 {
     "schemaVersion": "1.1",
     "aduShellTrustedUsers": [
@@ -451,125 +415,7 @@ function Install-Adu-Components {
     Register-Components
 }
 
-# For compatibility with SH script, emulate longopts
-$arg = 0
-while ($arg -lt $args.Count) {
-    switch ($args[$arg]) {
-        { $_ -in '-c', '--clean' } {
-            $build_clean = $true
-        }
-
-        { $_ -in '-t', '--type' } {
-            $arg++
-            if (-not $args[$arg]) {
-                Show-Error '-t build type parameter is mandatory.'
-                exit 1
-            }
-            $build_type = $args[$arg]
-        }
-
-        { $_ -in '-d', '--build-docs' } {
-            $build_documentation = $true
-        }
-
-        { $_ -in '-u', '--build-unit-tests' } {
-            $build_unittests = $true
-        }
-
-        '--build-packages' {
-            $build_packages = $true
-        }
-
-        { $_ -in '-o', '--out-dir' } {
-            $arg++
-            if (-not $args[$arg]) {
-                Show-Error '-o output directory parameter is mandatory.'
-                exit 1
-            }
-            $output_directory = $args[$arg]
-        }
-
-        { $_ -in '-s', '--static-analysis' } {
-            $arg++
-            if (-not $args[$arg]) {
-                Show-Error '-s static analysis tools parameter is mandatory.'
-                exit 1
-            }
-            if ($args[$arg] -eq 'all') {
-                $static_analysis_tools = @('clang-tidy', 'cppcheck', 'cpplint', 'iwyu', 'lwyu')
-            }
-            else {
-                $static_analysis_tools = $args[$arg] -split ','
-            }
-        }
-
-        { $_ -in '-p', '--platform-layer' } {
-            $arg++
-            if (-not $args[$arg]) {
-                Show-Error '-p platform layer parameter is mandatory.'
-                exit 1
-            }
-            $platform_layer = $args[$arg]
-        }
-
-        '--trace-target-deps' {
-            $trace_target_deps = $true
-        }
-
-        '--log-lib' {
-            $arg++
-            if (-not $args[$arg]) {
-                Show-Error '--log-lib parameter is mandatory.'
-                exit 1
-            }
-            $log_lib = $args[$arg]
-        }
-
-        { $_ -in '-l', '--log-dir' } {
-            $arg++
-            if (-not $args[$arg]) {
-                Show-Error '-l log directory  parameter is mandatory.'
-                exit 1
-            }
-            $adu_log_dir = $args[$arg]
-        }
-
-        '--install-prefix' {
-            $arg++
-            if (-not $args[$arg]) {
-                Show-Error '--install-prefix parameter is mandatory.'
-                exit 1
-            }
-            $install_prefix = $args[$arg]
-        }
-
-        '--install' {
-            $install_adu = $true
-        }
-
-        # '--cmake-path' {
-        #     $arg++
-        #     if (-not $args[$arg]) {
-        #         Show-Error '--cmake-path is mandatory.'
-        #         exit 1
-        #     }
-        #     $cmake_dir_path = $args[$arg]
-        # }
-
-        { $_ -in '-h', '--help' } {
-            print_help
-            exit 0
-        }
-
-        default {
-            Show-Error "Invalid argument: $($args[$arg])"
-            exit 1
-        }
-    }
-    $arg++
-}
-
-if ($build_documentation) {
+if ($BuildDocumentation) {
     if (-not (Get-Command 'doxygen.exe' -CommandType Application -ErrorAction SilentlyContinue)) {
         Show-Error 'Can''t build documentation - doxygen is not installed or not in PATH.'
         exit 1
@@ -581,72 +427,52 @@ if ($build_documentation) {
     }
 }
 
-# TODO(JeffMill): Add a 'windows' platform layer. simulator seems to be broken.
-if ($platform_layer -ne 'linux') {
-    Show-Error 'Only linux platform layer is supported currently!'
-    exit 1
-}
-
 # Set default log dir if not specified.
-if (-not $adu_log_dir) {
-    if ($platform_layer -eq 'simulator') {
-        Show-Warning "Forcing adu_log_dir to /tmp/aduc-logs"
-        $adu_log_dir = '/tmp/aduc-logs'
-    }
-    else {
-        $adu_log_dir = $default_log_dir
-    }
-}
-
-$runtime_dir = "$output_directory/bin"
-$library_dir = "$output_directory/lib"
+$runtime_dir = "$OutputDirectory/bin"
+$library_dir = "$OutputDirectory/lib"
 $cmake_bin = 'cmake.exe'
-# $shellcheck_bin = "$work_folder/deviceupdate-shellcheck"
+
+# TODO(JeffMill): This should be Windows when that plaform layer is ready.
+$PlatformLayer = 'linux'
 
 # Output banner
 ''
 Show-Header 'Building ADU Agent'
-Show-Bullet "Clean build: $build_clean"
-Show-Bullet "Documentation: $build_documentation"
-Show-Bullet "Platform layer: $platform_layer"
-Show-Bullet "Trace target deps: $trace_target_deps"
-# Show-Bullet "Content handlers: $content_handlers"
-Show-Bullet "Build type: $build_type"
-Show-Bullet "Log directory: $adu_log_dir"
-Show-Bullet "Logging library: $log_lib"
-Show-Bullet "Output directory: $output_directory"
-Show-Bullet "Build unit tests: $build_unittests"
-Show-Bullet "Build packages: $build_packages"
+Show-Bullet "Clean build: $Clean"
+Show-Bullet "Documentation: $BuildDocumentation"
+Show-Bullet "Platform layer: $PlatformLayer"
+Show-Bullet "Build type: $Type"
+Show-Bullet "Log directory: $LogDir"
+Show-Bullet "Logging library: $LogLib"
+Show-Bullet "Output directory: $OutputDirectory"
+Show-Bullet "Build unit tests: $BuildUnitTests"
+Show-Bullet "Build packages: $BuildPackages"
 Show-Bullet "CMake: $cmake_bin"
 Show-Bullet ("CMake version: {0}" -f (& $cmake_bin --version | Select-String  '^cmake version (.*)$').Matches.Groups[1].Value)
-# Show-Bullet "shellcheck: $shellcheck_bin"
-# Show-Bullet "shellcheck version: $("$shellcheck_bin" --version | grep 'version:' | awk '{ print $2 }')"
-if ($static_analysis_tools.Length -eq 0) {
+if ($StaticAnalysisTools.Length -eq 0) {
     Show-Bullet "Static analysis: (none)"
 }
 else {
-    Show-Bullet "Static analysis: $static_analysis_tools"
+    Show-Bullet "Static analysis: $StaticAnalysisTools"
 }
 ''
 
 # Store options for CMAKE in an array
 $CMAKE_OPTIONS = @(
-    "-DADUC_BUILD_DOCUMENTATION:BOOL=$build_documentation",
-    "-DADUC_BUILD_UNIT_TESTS:BOOL=$build_unittests",
-    "-DADUC_BUILD_PACKAGES:BOOL=$build_packages",
-#    "-DADUC_CONTENT_HANDLERS:STRING=$content_handlers",
-    "-DADUC_LOG_FOLDER:STRING=$adu_log_dir",
-    "-DADUC_LOGGING_LIBRARY:STRING=$log_lib",
-    "-DADUC_PLATFORM_LAYER:STRING=$platform_layer",
-    "-DADUC_TRACE_TARGET_DEPS=$trace_target_deps",
-    "-DCMAKE_BUILD_TYPE:STRING=$build_type",
+    "-DADUC_BUILD_DOCUMENTATION:BOOL=$BuildDocumentation",
+    "-DADUC_BUILD_UNIT_TESTS:BOOL=$BuildUnitTests",
+    "-DADUC_BUILD_PACKAGES:BOOL=$BuildPackages",
+    "-DADUC_LOG_FOLDER:STRING=$LogDir",
+    "-DADUC_LOGGING_LIBRARY:STRING=$LogLib",
+    "-DADUC_PLATFORM_LAYER:STRING=$PlatformLayer",
+    "-DCMAKE_BUILD_TYPE:STRING=$Type",
     "-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON",
     "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY:STRING=$library_dir",
     "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY:STRING=$runtime_dir",
-    "-DCMAKE_INSTALL_PREFIX=$install_prefix"
+    "-DCMAKE_INSTALL_PREFIX=$InstallPrefix"
 )
 
-$static_analysis_tools | ForEach-Object {
+$StaticAnalysisTools | ForEach-Object {
     switch ($_) {
         'clang-tidy' {
             # Part of VS Build Tools'
@@ -673,18 +499,18 @@ $static_analysis_tools | ForEach-Object {
         }
 
         'cpplint' {
-            Show-Error 'cpplint NYI'
-            exit 1
+            # TODO(JefFMill): Implement cpplint
+            Show-Warning 'Static analysis tool cpplint NYI'
         }
 
         'iwyu' {
-            Show-Error 'iwyu NYI'
-            exit 1
+            # TODO(JefFMill): Implement iwyu
+            Show-Warning 'Static analysis tool iwyu NYI'
         }
 
         'lwyu' {
-            Show-Error 'lwyu NYI'
-            exit 1
+            # TODO(JefFMill): Implement lwyu
+            Show-Warning 'Static analysis tool lwyu NYI'
         }
 
         default {
@@ -693,25 +519,25 @@ $static_analysis_tools | ForEach-Object {
     }
 }
 
-if (-not $build_clean) {
+if (-not $Clean) {
     # -ErrorAction SilentlyContinue doesn't work on Select-String
     try {
-        if (-not (Select-String 'CMAKE_PROJECT_NAME:' "$output_directory/CMakeCache.txt")) {
-            $build_clean = $true
+        if (-not (Select-String 'CMAKE_PROJECT_NAME:' "$OutputDirectory/CMakeCache.txt")) {
+            $Clean = $true
         }
     }
     catch {
-        $build_clean = $true
+        $Clean = $true
     }
 
-    if ($build_clean) {
+    if ($Clean) {
         Show-Warning 'CMake cache seems out of date - forcing clean build.'
         ''
-        $build_clean = $true
+        $Clean = $true
     }
 }
 
-if ($build_clean) {
+if ($Clean) {
     Show-Header 'Cleaning repo'
 
     $decision = $Host.UI.PromptForChoice(
@@ -728,9 +554,9 @@ if ($build_clean) {
     }
 
 
-    if (Test-Path $output_directory) {
-        Show-Bullet $output_directory
-        Remove-Item -Recurse -LiteralPath $output_directory
+    if (Test-Path $OutputDirectory) {
+        Show-Bullet $OutputDirectory
+        Remove-Item -Recurse -LiteralPath $OutputDirectory
     }
     # TODO(JeffMill): This shouldn't hard code /tmp/adu/testdata -- it should match
     # whatever cmake and tests have.  JW has a bug on this.
@@ -742,11 +568,11 @@ if ($build_clean) {
     ''
 }
 
-mkdir -Path $output_directory -Force | Out-Null
+mkdir -Path $OutputDirectory -Force | Out-Null
 
 # TODO(JeffMill): Only generate makefiles on clean?
 # It seems that dependencies are being generated unnecessarily otherwise?
-if ($build_clean) {
+if ($Clean) {
     Show-Header 'Generating Makefiles'
 
     # show every find_package call (vcpkg specific):
@@ -766,9 +592,7 @@ if ($build_clean) {
 
     "CMAKE_OPTIONS:`n  {0}`n" -f ($CMAKE_OPTIONS -join "`n  ")
 
-    & $cmake_bin -S "$root_dir" -B $output_directory @CMAKE_OPTIONS 2>&1 | Tee-Object -Variable cmake_output
-    # TODO(JeffMill): Scenario 2: Use Ninja
-    # & $cmake_bin -S "$root_dir" -B $output_directory  -G Ninja @CMAKE_OPTIONS
+    & $cmake_bin -S "$root_dir" -B $OutputDirectory @CMAKE_OPTIONS 2>&1 | Tee-Object -Variable cmake_output
     $ret_val = $LASTEXITCODE
 
     if ($ret_val -ne 0) {
@@ -792,13 +616,11 @@ Show-Header 'Delivery Optimization Library'
 
 # Reusing ".vcpkg-installed" folder ... why not?
 $DoPath = "{0}/.vcpkg-installed/do-client" -f (git.exe rev-parse --show-toplevel)
-Install-DeliveryOptimization -BuildType $build_type -Path $DoPath -Branch 'develop' -Commit 'ad16298e247480c6ed034c957cc3649c75ae6a8c'
+Install-DeliveryOptimization -Type $Type -Path $DoPath -Branch 'develop' -Commit 'ad16298e247480c6ed034c957cc3649c75ae6a8c'
 
 Show-Header 'Building Product'
 
-# TODO(JeffMill): Scenario 2: Use Ninja
-
-& $cmake_bin --build $output_directory --config $build_type --parallel 2>&1 | Tee-Object -Variable build_output
+& $cmake_bin --build $OutputDirectory --config $Type --parallel 2>&1 | Tee-Object -Variable build_output
 $ret_val = $LASTEXITCODE
 ''
 
@@ -815,12 +637,14 @@ if ($ret_val -ne 0) {
     exit $ret_val
 }
 
-# if ($ret_val -eq 0 -and $build_packages) {
-#     cpack
-#     $ret_val=$LASTEXITCODE
-# }
+if ($ret_val -eq 0 -and $BuildPackages) {
+    Show-Header 'Building Package'
 
-if ($ret_val -eq 0 -and $install_adu) {
+    # TODO(JeffMill): Build package for Windows
+    Show-Warning 'BuildPackages NYI'
+}
+
+if ($ret_val -eq 0 -and $Install) {
     Install-Adu-Components
 }
 
--- scripts/debug_agent.ps1
+++ scripts/debug_agent.ps1
@@ -1,10 +1,24 @@
 Param(
+    # Filename to debug.
     [Parameter(Position = 0)][string]$FileName = 'AducIotAgent',
+    # Set to build type, e.g. Debug, Release.
     [string]$Type = 'Debug',
-    # Set this to true if debugging a Catch2 Unit Test.
+    # Set to true if debugging a Catch2 Unit Test.
     [switch]$DebugCatchUT = $false,
-    [switch]$NoDebugger = $false
+    # Set to true if process launch without debugging is desired.
+    [switch]$NoDebugger = $false,
+    # Additional arguments to pass to command line.
+    [string[]] $ArgumentList
 )
+
+# Example ArgumentList for AducIotAgent:
+# '--register-extension', '/var/lib/adu/extensions/sources/microsoft_steps_1.dll', '--extension-type', 'updateContentHandler', '--extension-id', 'microsoft/steps:1'
+# "--health-check"
+
+# Example ArgumentList for adu-shell:
+# '--update-type', 'common', '--update-action', 'reboot'
+
+
 
 function Show-Error {
     Param([Parameter(mandatory = $true, position = 0)][string]$Message)
@@ -63,6 +77,7 @@ if ($NoDebugger) {
     & $app
 }
 else {
+
     try {
         $windbgx = Get-WinDbgX
     }
@@ -72,15 +87,17 @@ else {
         exit 1
     }
 
+    $module = $FileName -replace '-','_'
+
     if ($DebugCatchUT) {
         $InitialCommands = @( `
                 # e.g. https_proxy_utils_unit_tests!`anonymous namespace'::C_A_T_C_H_T_E_S_T_0::test
-                '-c', "bm $FileName!*::C_A_T_C_H_T_E_S_T_*::test", `
+                '-c', "bm $module!*::C_A_T_C_H_T_E_S_T_*::test", `
                 # e.g. permission_utils_unit_test!C_A_T_C_H_T_E_S_T_0
-                '-c', "bm $FileName!C_A_T_C_H_T_E_S_T_*", `
+                '-c', "bm $module!C_A_T_C_H_T_E_S_T_*", `
                 # e.g. https_proxy_utils_unit_tests!TestCaseFixture::TestCaseFixture
-                '-c', "bm $FileName!TestCaseFixture::TestCaseFixture", `
-                '-c', "bm $FileName!TestCaseFixture::~TestCaseFixture", `
+                '-c', "bm $module!TestCaseFixture::TestCaseFixture", `
+                '-c', "bm $module!TestCaseFixture::~TestCaseFixture", `
                 # Start test
                 '-c', 'g' `
         )
@@ -89,14 +106,9 @@ else {
     }
     else {
         $InitialCommands = @( `
-                '-c', "bp $FileName!main", `
+                '-c', "bp $module!main", `
                 '-c', 'g' `
         )
-
-        # TODO: Allow passing add'l args via commandline, e.g.
-        # '--register-extension', '/var/lib/adu/extensions/sources/curl_content_downloader.dll', '--extension-type', 'contentDownloader'
-        # '--register-extension', '/var/lib/adu/extensions/sources/microsoft_steps_1.dll', '--extension-type', 'updateContentHandler', '--extension-id', 'microsoft/steps:1'
-        # "--health-check"
 
         $arguments = @( `
             # Log level is DEBUG (very verbose) -- useful for debugging.
@@ -104,8 +116,8 @@ else {
         )
     }
 
-    'Debugging: {0} {1}' -f $app, ($arguments -join ' ')
-    cmd.exe /c start $windbgx @InitialCommands $app @arguments
+    'Debugging: {0} {1} {2}' -f $app, ($arguments -join ' '), ($ArgumentList -join ' ')
+    cmd.exe /c start $windbgx @InitialCommands $app @arguments @ArgumentList
 }
 
 exit 0
