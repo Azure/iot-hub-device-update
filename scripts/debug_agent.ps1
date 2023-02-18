3c4da9619a0f9a18839f2d20051dcb08b407774c
Jeff Miller <jeffmill@microsoft.com>
2023-02-17 16:12:59 -0800

    Support elevated debugging.

--- scripts/debug_agent.ps1
+++ scripts/debug_agent.ps1
@@ -1,14 +1,16 @@
 Param(
     # Filename to debug.
     [Parameter(Position = 0)][string]$FileName = 'AducIotAgent',
-    # Set to build type, e.g. Debug, Release.
+    # Additional appArgs to pass to command line.
+    [string[]] $ArgumentList,
+    # Build type, e.g. Debug, Release.
     [string]$Type = 'Debug',
     # Set to true if debugging a Catch2 Unit Test.
     [switch]$DebugCatchUT = $false,
     # Set to true if process launch without debugging is desired.
     [switch]$NoDebugger = $false,
-    # Additional appArgs to pass to command line.
-    [string[]] $ArgumentList
+    # Run the process elevated?
+    [switch]$Elevated = $false
 )
 
 # Example ArgumentList for AducIotAgent:
@@ -17,8 +19,6 @@ Param(
 
 # Example ArgumentList for adu-shell:
 # '--update-type', 'common', '--update-action', 'reboot'
-
-
 
 function Show-Error {
     Param([Parameter(mandatory = $true, position = 0)][string]$Message)
@@ -52,7 +52,12 @@ if (-not (Test-Path $app -PathType Leaf)) {
 
 if ($NoDebugger) {
     "Launching $app . . ."
-    & $app
+    if (-not $Elevated) {
+        & $app
+    }
+    else {
+        Start-Process -FilePath $app -Verb RunAs
+    }
 }
 else {
     if (-not (Get-Command -Name 'WinDbgX.exe' -CommandType Application)) {
@@ -66,32 +71,44 @@ else {
     if ($DebugCatchUT) {
         # Debugging a Catch2 Unit Test
 
-        $debuggerArgs = @( `
-                # e.g. https_proxy_utils_unit_tests!`anonymous namespace'::C_A_T_C_H_T_E_S_T_0::test
-                '-c', "bm $module!*::C_A_T_C_H_T_E_S_T_*::test", `
-                # e.g. permission_utils_unit_test!C_A_T_C_H_T_E_S_T_0
-                '-c', "bm $module!C_A_T_C_H_T_E_S_T_*", `
-                # e.g. https_proxy_utils_unit_tests!TestCaseFixture::TestCaseFixture
-                '-c', "bm $module!TestCaseFixture::TestCaseFixture", `
-                '-c', "bm $module!TestCaseFixture::~TestCaseFixture", `
-                # Start test
-                '-c', 'g' `
-        )
+        $debuggerArgs = `
+            # e.g. https_proxy_utils_unit_tests!`anonymous namespace'::C_A_T_C_H_T_E_S_T_0::test
+            '-c', "`"bm $module!*::C_A_T_C_H_T_E_S_T_*::test`"", `
+            # e.g. permission_utils_unit_test!C_A_T_C_H_T_E_S_T_0
+            '-c', "`"bm $module!C_A_T_C_H_T_E_S_T_*`"", `
+            # e.g. https_proxy_utils_unit_tests!TestCaseFixture::TestCaseFixture
+            '-c', "`"bm $module!TestCaseFixture::TestCaseFixture`"", `
+            '-c', "`"bm $module!TestCaseFixture::~TestCaseFixture`"", `
+            # Start test
+            '-c', 'g'
 
-        $appArgs = @("--success", "--break")
+        $appArgs = "--success", "--break"
     }
     else {
         # Debugging an .EXE
         # -o : Debugs all processes launched by the target application (child processes).
 
-        $debuggerArgs = @( '-c', "bp $module!main", '-c', 'g' )
+        $debuggerArgs = '-c', "`"bp $module!main`"", '-c', 'g'
 
         # Log level is DEBUG (very verbose) -- useful for debugging.
-        $appArgs = @( '--log-level', '0' )
+        $appArgs = '--log-level', '0'
     }
 
-    'Debugging: {0} {1} {2}' -f $app, ($appArgs -join ' '), ($ArgumentList -join ' ')
-    WinDbgX.exe @debuggerArgs $app @appArgs @ArgumentList
+    $argList = $debuggerArgs
+    $argList += $app
+    $argList += $appArgs
+    if ($ArgumentList) {
+        $argList += $ArgumentList
+    }
+
+    "`nWinDbgX.exe $argList`n"
+
+    if (-not $Elevated) {
+        WinDbgX.exe $argList
+    }
+    else {
+        Start-Process  -FilePath 'WinDbgX.exe' -ArgumentList $argList -Verb RunAs
+    }
 }
 
 exit 0
