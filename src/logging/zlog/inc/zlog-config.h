31c99b8599e4dea9a47b00d7b3799829273ac349
jw-msft <84477130+jw-msft@users.noreply.github.com>
2023-03-22 13:29:33 -0700

    Add comments how values were derived

--- src/logging/zlog/inc/zlog-config.h
+++ src/logging/zlog/inc/zlog-config.h
@@ -15,6 +15,18 @@
 // enabling this will slow down the log
 // #define ZLOG_FORCE_FLUSH_BUFFER
 
+// The following values for line maxchars and maxlines for zlog buffer were
+// derived by looking at arithmetic mean and standard deviation of chars/lines
+// across all log files in the log dir after a script-based download and
+// install of a 1 GB update payload.
+// Average line length usually did not exceed 200 chars and adding in std
+// deviation, it was roughly around 300 chars, so 328 (41 * 8) adds some safety
+// margin to avoid unnecessary flushing + direct FD write in most cases.
+// Most logs had lines between a dozen to 64 lines, but du-agent.* logs were
+// in the range of 100-300, so 128 was a nice balance between keeping working
+// set smaller (41 KB) and not flushing too often but often enough that the log
+// traces on disk felt responsive when tailing the logs and being available for
+// diagnostics to upload.
 #define ZLOG_BUFFER_LINE_MAXCHARS 328
 #define ZLOG_BUFFER_MAXLINES 128
 
