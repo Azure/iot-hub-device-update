b39a011a387936927f64630ea1f2a33195072802
Shiyi Peng <shiyipeng@microsoft.com>
2023-04-06 14:34:19 -0700

    git checkout a commit

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -72,7 +72,7 @@ install_githooks=false
 
 du_test_data_dir_path="/tmp/adu/"
 # DO Deps
-default_do_ref=02c9ae2c7484182903c66ad986a834762fc569e6
+default_do_ref=develop
 install_do=false
 do_ref=$default_do_ref
 
@@ -444,6 +444,8 @@ do_install_do() {
 
     git clone --recursive --single-branch --branch $do_ref --depth 1 $do_url . || return
 
+    #TODO: Remove the sepecif commit once DO publishs a new tag
+    git checkout 02c9ae2c7484182903c66ad986a834762fc569e6
     bootstrap_file=$do_dir/build/scripts/bootstrap.sh
     chmod +x $bootstrap_file || return
     $SUDO $bootstrap_file --install build || return
