8c68392c64e8692dacd85db86ff7f97bad79e3e6
Shiyi Peng <shiyipeng@microsoft.com>
2023-05-19 21:06:25 -0700

    remove verify_user_group_permissions

--- azurepipelines/e2e_test/scenarios/test_runner/vm_setup/setup.sh
+++ azurepipelines/e2e_test/scenarios/test_runner/vm_setup/setup.sh
@@ -305,7 +305,7 @@ register_extensions
 # this test will be considered a failure.
 sudo systemctl restart deviceupdate-agent.service
 
-verify_user_group_permissions
+#verify_user_group_permissions
 
 verify_log_files
 
