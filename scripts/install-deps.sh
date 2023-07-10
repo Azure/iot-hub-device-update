9771848dd366e209274f4431e55be007099aff49
jw-msft <84477130+jw-msft@users.noreply.github.com>
2023-07-10 16:25:52 -0700

    Revert "add libxml2-dev for codeql github action"
    This reverts commit 37215cf11db5eb37ca933178e67e8d129ca9f7d7.

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -76,7 +76,7 @@ install_do=false
 do_ref=$default_do_ref
 
 # Dependencies packages
-aduc_packages=('git' 'make' 'build-essential' 'cmake' 'ninja-build' 'libcurl4-openssl-dev' 'libssl-dev' 'uuid-dev' 'python2.7' 'lsb-release' 'curl' 'wget' 'pkg-config' 'libxml2-dev')
+aduc_packages=('git' 'make' 'build-essential' 'cmake' 'ninja-build' 'libcurl4-openssl-dev' 'libssl-dev' 'uuid-dev' 'python2.7' 'lsb-release' 'curl' 'wget' 'pkg-config')
 static_analysis_packages=('clang' 'clang-tidy' 'cppcheck')
 compiler_packages=("gcc-[68]")
 do_packages=('libproxy-dev' 'libssl-dev' 'libboost-all-dev')
