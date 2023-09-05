7ed59320cc1e5b3c8766b09e1a28a5d87ef0321e
root <root@DESKTOP-G6CSMHJ.localdomain>
2023-09-05 00:24:05 -0700

    ubuntu 22

--- azurepipelines/e2e_test/scenarios/test_runner/vm_setup/setup.sh
+++ azurepipelines/e2e_test/scenarios/test_runner/vm_setup/setup.sh
@@ -150,6 +150,12 @@ function install_do() {
         elif [ "$version" == "20.04" ] && [ "$architecture" == "arm64" ]; then
             package_url="https://github.com/microsoft/do-client/releases/download/v1.0.0/ubuntu2004_arm64-packages.tar"
             package_filename="ubuntu20_arm64-packages.tar"
+        elif [ "$version" == "22.04" ] && [ "$architecture" == "amd64" ]; then
+            package_url="https://github.com/microsoft/do-client/releases/download/v1.0.2/ubuntu2204_x64-packages.tar"
+            package_filename="ubuntu22_x64-packages.tar"
+        elif [ "$version" == "22.04" ] && [ "$architecture" == "arm64" ]; then
+            package_url="https://github.com/microsoft/do-client/releases/download/v1.0.2/ubuntu2204_arm64-packages.tar"
+            package_filename="ubuntu22_arm64-packages.tar"
         fi
     elif [ "$distro" == "debian" ] && [ "$version" == "10" ] && [ "$architecture" == "amd64" ]; then
         package_url="https://github.com/microsoft/do-client/releases/download/v1.0.0/debian10_x64-packages.tar"
