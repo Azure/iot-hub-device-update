b2c890f1782b5196be2e07748607e4efc9ce78e3
nihemstr <nihemstr@microsoft.com>
2023-09-21 20:43:16 -0500

    fixing minor errors

--- tools/openssl_3_install_script/install-openssl.sh
+++ tools/openssl_3_install_script/install-openssl.sh
@@ -30,6 +30,8 @@ error() { echo -e "\033[1;31mError:\033[0m $*" >&2; }
 
 openssl_dir_path="/usr/local/lib/deviceupdate-openssl"
 openssl_version="3.1.2"
+DEFAULT_WORKFOLDER=/tmp
+work_folder=$DEFAULT_WORKFOLDER
 
 print_help() {
     echo "Usage: install-openssl3.sh [options...]"
@@ -53,9 +55,9 @@ do_install_openssl() {
 
     $SUDO apt-get install -y build-essential checkinstall zlib1g-dev -y || return
     wget https://www.openssl.org/source/openssl-${openssl_version}.tar.gz || return
-    mkdir -p $openssl_dir || return
-    tar -xf openssl-${openssl_version}.tar.gz -C $openssl_dir --strip-components=1 || return
-    pushd $openssl_dir > /dev/null || return
+    mkdir -p "$openssl_dir" || return
+    tar -xf openssl-${openssl_version}.tar.gz -C "$openssl_dir" --strip-components=1 || return
+    pushd "$openssl_dir" > /dev/null || return
     ./config --prefix="$openssl_dir_path" --openssldir="$openssl_dir_path" shared zlib || return
     make || return
 
@@ -65,8 +67,10 @@ do_install_openssl() {
 
     popd > /dev/null || return
 
-    local openssl_major_version=$(echo $openssl_version | cut -d'.' -f1)
-    local openssl_minor_version=$(echo $openssl_version | cut -d'.' -f2)
+    local openssl_major_version=""
+    openssl_major_version=$(echo $openssl_version | cut -d'.' -f1)
+    local openssl_minor_version=""
+    openssl_minor_version=$(echo $openssl_version | cut -d'.' -f2)
 
     local openssl_so_suffix=""
     if [[ $openssl_major_version -eq "1" && $openssl_minor_version -eq "1" ]]; then
@@ -84,7 +88,7 @@ do_install_openssl() {
     ## if the openssl version is 3.1 it's
     echo "Creating legacy lib/libcrypto.so and lib/libssl.so symlink for backward compatibility"
 
-    if [[! -d "$openssl_dir_path/lib" ]]; then
+    if [[ ! -d "$openssl_dir_path/lib" ]]; then
         # Handle missing /lib/ directory
         $SUDO mkdir $openssl_dir_path/lib || $ret 1
         $SUDO ln -sf "$openssl_dir_path/lib64/libcrypto.so.$openssl_so_suffix" "$openssl_dir_path/lib/libcrypto.so" || return
@@ -116,8 +120,8 @@ do_install_openssl() {
     $SUDO ldconfig $openssl_dir_path/lib64/ || return
     $SUDO ldconfig $openssl_dir_path/lib/ || return
 
-    $SUDO rm -r $openssl_dir || return
-    $SUDO rm openssl-${openssl_version}.tar.gz || return
+    $SUDO rm -r "$openssl_dir" || return
+    $SUDO rm "openssl-${openssl_version}.tar.gz" || return
 }
 
 ###############################################################################
