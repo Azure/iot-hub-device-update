4c5fa6fd5ce5ac7981db2283c0be92b1efb8a5dc
jw-msft <84477130+jw-msft@users.noreply.github.com>
2023-11-01 00:33:34 +0000

    Remove Diagnostics as unneeded for mvp

--- src/extensions/agent_modules/cloud_communication/iothub_client/diagnostics_component/utils/file_info_utils/tests/main.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file main.cpp
- * @brief file_info_utils_ut tests main entry point.
- *
- * @copyright Copyright (c) Microsoft Corporation.
- * Licensed under the MIT License.
- */
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
