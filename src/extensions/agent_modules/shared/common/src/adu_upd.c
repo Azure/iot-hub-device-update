5ebb1f8dfd0b1e05c4e415a6d4ec9566f7263d21
jw-msft <84477130+jw-msft@users.noreply.github.com>
2023-12-04 09:58:46 -0800

    add adu_upd

---
+++ src/extensions/agent_modules/shared/common/src/adu_upd.c
@@ -0,0 +1,34 @@
+#include "aduc/adu_upd.h"
+
+/**
+ * @brief Gets the string representation of the update module state.
+ *
+ * @param updateModuleState The update module state.
+ * @return const char* The string form of the update module state.
+ */
+const char* AduUpdState_str(ADU_UPD_STATE updateModuleState)
+{
+    switch (updateModuleState)
+    {
+    case ADU_UPD_STATE_READY:
+        return "Ready";
+
+    case ADU_UPD_STATE_IDLEWAIT:
+        return "Idle Wait";
+
+    case ADU_UPD_STATE_REQUESTING:
+        return "Requesting";
+
+    case ADU_UPD_STATE_RETRYWAIT:
+        return "Retry Wait";
+
+    case ADU_UPD_STATE_REQUEST_ACK:
+        return "Request Acknowledged";
+
+    case ADU_UPD_STATE_PROCESSING_UPDATE:
+        return "Processing Update";
+
+    default:
+        return "???";
+    }
+}
