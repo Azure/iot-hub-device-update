da78109d51ad2a11965c84fe9bbbdc6fecbe11bd
jw-msft <84477130+jw-msft@users.noreply.github.com>
2024-01-20 17:28:16 +0000

    use dollar SUDO for installing mosquitto ppa

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -87,7 +87,7 @@ catch2_cxx=""
 
 # Dependencies packages
 # For libmosquitto-dev
-sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa -y
+$SUDO apt-add-repository ppa:mosquitto-dev/mosquitto-ppa -y
 aduc_packages=('git' 'make' 'build-essential' 'cmake' 'ninja-build' 'libcurl4-openssl-dev' 'libssl-dev' 'libxml2-dev' 'uuid-dev' 'python2.7' 'lsb-release' 'curl' 'wget' 'pkg-config' 'libmosquitto-dev')
 static_analysis_packages=('clang' 'clang-tidy' 'cppcheck')
 compiler_packages=('gcc' 'g++')
