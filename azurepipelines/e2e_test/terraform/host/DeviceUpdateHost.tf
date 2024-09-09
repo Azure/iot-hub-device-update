5a09405afad9beb01adf0680407d5c13a6a3d814
jw-msft <84477130+jw-msft@users.noreply.github.com>
2024-09-09 11:45:29 -0700

    add AzureMonitorLinuxAgent vm extension to az vm terraform

--- azurepipelines/e2e_test/terraform/host/DeviceUpdateHost.tf
+++ azurepipelines/e2e_test/terraform/host/DeviceUpdateHost.tf
@@ -193,3 +193,18 @@ resource "azurerm_dev_test_global_vm_shutdown_schedule" "vm_shutdown_schedule" {
       enabled = false
   }
 }
+
+resource "azurerm_virtual_machine_extension" "AzureMonitorLinuxAgent" {        
+  name                       = "AzureMonitorLinuxAgent"
+  publisher                  = "Microsoft.Azure.Monitor"
+  type                       = "AzureMonitorLinuxAgent"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = "true"
+  virtual_machine_id = azurerm_linux_virtual_machine.deviceupdatevm.id
+  settings = <<SETTINGS
+  {
+      "commandToExecute": "hostname && uptime"
+  }
+SETTINGS
+
+}
