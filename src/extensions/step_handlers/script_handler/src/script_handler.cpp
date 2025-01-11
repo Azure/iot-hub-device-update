bdbc5be0e1dcfe647e9ff7e58b57633165c1695b
Shiyi Peng <shiyipeng@microsoft.com>
2025-01-10 16:57:27 -0800

    add script handler script permission

--- src/extensions/step_handlers/script_handler/src/script_handler.cpp
+++ src/extensions/step_handlers/script_handler/src/script_handler.cpp
@@ -21,6 +21,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <sys/stat.h> // for chmod
 
 // keep this last to avoid interfering with system headers
 #include "aduc/aduc_banned.h"
@@ -155,6 +156,12 @@ static ADUC_Result Script_Handler_DownloadPrimaryScriptFile(ADUC_WorkflowHandle 
     try
     {
         result = ExtensionManager::Download(&entity, handle, &Default_ExtensionManager_Download_Options, nullptr);
+        if (IsAducResultCodeSuccess(result.ResultCode))
+        {
+            // Set execute permissions on the downloaded script file
+            std::string scriptPath = std::string(workFolder) + "/" + scriptFileName;
+            chmod(scriptPath.c_str(), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+        }
     }
     catch (...)
     {
