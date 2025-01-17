70c13863837b4c848a8d718c86e30426ef5182ef
jw-msft <84477130+jw-msft@users.noreply.github.com>
2025-01-17 11:44:12 -0800

    clangfmt, add readme, fix off by one arg

---
+++ src/utils/download_file/README.md
@@ -0,0 +1,53 @@
+# Usage
+
+Usage: ./out/bin/download_file (--rootkeypkg-download|--payload-download) [--simulate-bad-hash] <URL> <FILEPATH>
+
+Example:
+
+```sh
+sudo -u adu ./out/bin/download_file --rootkeypkg-download <MY URL> <
+```
+
+# Build
+
+./src/build.sh -c
+
+# Run
+
+## Simulate DO 404 Not Found for rootkey package
+
+```
+sudo -u adu ./out/bin/download_file --rootkeypkg-download http://catchpoint.b.nlu.dl.adu.microsoft.com/westeurope/rootkeypackages/rootkeypackage-1.json.DOESNOTEXIST /tmp/rootkeypackage-1.json
+```
+
+Output:
+
+```
+Parsed URL arg: 'http://catchpoint.b.nlu.dl.adu.microsoft.com/westeurope/rootkeypackages/rootkeypackage-1.json.DOESNOTEXIST'
+Parsed FILE arg: '/tmp/rootkeypackage-1.json'
+DownloadRootKeyPkg_DO - Downloading File 'http://catchpoint.b.nlu.dl.adu.microsoft.com/westeurope/rootkeypackages/rootkeypackage-1.json.DOESNOTEXIST' to '/tmp/rootkeypackage-1.json'
+DO error, msg: unrecognized error, code: 0x80190194, timeout? 0
+Download rc: 0, erc: 0xa0190194
+ResultCode: 0(0x00000000)
+ERC: -1608973932(0xa0190194)
+```
+
+### Example: /var/log/deliveryoptimization-agent/do-agent.20250110_194623.log
+
+```
+2025-01-15T00:49:30.3076606Z 47127 47130          {TraceDownloadStatus} id: db4dd540-29fd-b648-9898-43ba09f04ec0, 5, codes: [404, 0x80190194, 0x0], 0 / 0
+2025-01-15T00:49:30.3080816Z 47127 47129 info     {ParseAndProcess} Download state change: 5
+2025-01-15T00:49:30.3081585Z 47127 47130          {_PerformStateChange} db4dd540-29fd-b648-9898-43ba09f04ec0, state change request 5 --> 4
+2025-01-15T00:49:30.3082808Z 47127 47130 info     {Delete} (hr:0) Delete file /tmp/rootkeypackage-1.json
+2025-01-15T00:49:30.3082853Z 47127 47130          {TraceDownloadStatus} id: db4dd540-29fd-b648-9898-43ba09f04ec0, 4, codes: [404, 0x80190194, 0x0], 0 / 0
+2025-01-15T00:49:30.3082886Z 47127 47130 info     {TraceDownloadCanceled} (hr:80190194) id: db4dd540-29fd-b648-9898-43ba09f04ec0, extError: 0, cdnUrl: http://
+catchpoint.b.nlu.dl.adu.microsoft.com/westeurope/rootkeypackages/rootkeypackage-1.json.DOESNOTEXIST, mccHost: , filePath: /tmp/rootkeypackage-1.json, bytes: [
+total: 0, down: 0]
+```
+
+## Simulate DO 200 OK
+
+```sh
+rm -f /tmp/rootkeypackage*.json
+sudo -u adu ./out/bin/download_file --rootkeypkg-download http://catchpoint.b.nlu.dl.adu.microsoft.com/westeurope/rootkeypackages/rootkeypackage-1.json /tmp/rootkeypackage-1.json
+```
--- src/utils/download_file/main.cpp
+++ src/utils/download_file/main.cpp
@@ -3,8 +3,8 @@
 #include <do_config.h>
 #include <do_download.h>
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -13,8 +13,9 @@ ADUC_Result DownloadRootKeyPkg_DO(const char* url, const char* targetFilePath);
 
 //decls
 typedef int32_t ADUC_Result_t;
-typedef enum {
-    SimMode_NONE=0,
+typedef enum
+{
+    SimMode_NONE = 0,
     SimMode_DOWNLOAD_ROOTKEY_PKG,
     SimMode_DOWNLOAD_UPDATE_PAYLOAD,
 } SimMode;
@@ -24,37 +25,48 @@ SimMode g_SimMode = SimMode_NONE;
 char* g_arg_url = NULL;
 char* g_arg_out_filepath = NULL;
 
-bool parse_args(int argc, char**argv) {
+bool parse_args(int argc, char** argv)
+{
     bool parsed = false;
     // argv[0] (--rootkeypkg-download|--payload-download) [--simulate-bad-hash] <URL> <FILEPATH>
     int non_switch_cnt = 0;
-    for(int i=1; i<argc-1; ++i) {
-        if(0 == strcmp(argv[i], "--rootkeypkg-download")) {
+    for (int i = 1; i < argc; ++i)
+    {
+        if (0 == strcmp(argv[i], "--rootkeypkg-download"))
+        {
             g_SimMode = SimMode_DOWNLOAD_ROOTKEY_PKG;
-        } else if(0 == strcmp(argv[i], "--rootkeypkg-download")) {
+        }
+        else if (0 == strcmp(argv[i], "--rootkeypkg-download"))
+        {
             g_SimMode = SimMode_DOWNLOAD_UPDATE_PAYLOAD;
-        } else {
+        }
+        else
+        {
             size_t len = strlen(argv[i]);
             ++non_switch_cnt;
-            if(non_switch_cnt > 2) {
+            if (non_switch_cnt > 2)
+            {
                 fprintf(stderr, "Too many non-switch args!\n");
                 goto done;
             }
-            switch(non_switch_cnt) {
+            switch (non_switch_cnt)
+            {
             case 1:
                 g_arg_url = (char*)calloc(1 + len, 1);
-                if(NULL == g_arg_url) {
+                if (NULL == g_arg_url)
+                {
                     goto done;
                 }
-                strncpy(g_arg_url/*dest*/, argv[i]/*src*/, len);
+                strncpy(g_arg_url /*dest*/, argv[i] /*src*/, len);
                 printf("Parsed URL arg: '%s'\n", g_arg_url);
                 break;
             case 2:
                 g_arg_out_filepath = (char*)calloc(1 + len, 1);
-                if(NULL == g_arg_out_filepath) {
+                if (NULL == g_arg_out_filepath)
+                {
                     goto done;
                 }
-                strncpy(g_arg_out_filepath/*dest*/, argv[i]/*src*/, len);
+                strncpy(g_arg_out_filepath /*dest*/, argv[i] /*src*/, len);
                 printf("Parsed FILE arg: '%s'\n", g_arg_out_filepath);
                 break;
             default:
@@ -67,31 +79,47 @@ done:
     return parsed;
 }
 
-void usage(const char* argv0) {
-    fprintf(stderr, "\nUsage: %s (--rootkeypkg-download|--payload-download) [--simulate-bad-hash] <URL> <FILEPATH>\n\n", argv0);
+void usage(const char* argv0)
+{
+    fprintf(
+        stderr,
+        "\nUsage: %s (--rootkeypkg-download|--payload-download) [--simulate-bad-hash] <URL> <FILEPATH>\n\n",
+        argv0);
 }
 
 int main(int argc, char** argv)
 {
     ADUC_Result result = {};
 
-    if (!parse_args(argc,argv)) {
+    if (!parse_args(argc, argv))
+    {
         usage(argv[0]);
         exit(1);
     }
 
-    if(g_SimMode == SimMode_DOWNLOAD_ROOTKEY_PKG) {
+    if (g_SimMode == SimMode_DOWNLOAD_ROOTKEY_PKG)
+    {
         result = DownloadRootKeyPkg_DO(g_arg_url, g_arg_out_filepath);
-    } else if (g_SimMode == SimMode_DOWNLOAD_UPDATE_PAYLOAD) {
+    }
+    else if (g_SimMode == SimMode_DOWNLOAD_UPDATE_PAYLOAD)
+    {
         // TODO
         fprintf(stderr, "\nDownload Update Payload NOT SUPPORTED YET\n");
     }
 
-    printf("ResultCode: %d(0x%08x)\nERC: %d(0x%08x)\n", result.ResultCode, result.ResultCode, result.ExtendedResultCode, result.ExtendedResultCode);
+    printf(
+        "ResultCode: %d(0x%08x)\nERC: %d(0x%08x)\n",
+        result.ResultCode,
+        result.ResultCode,
+        result.ExtendedResultCode,
+        result.ExtendedResultCode);
 
-    if(IsAducResultCodeFailure(result.ResultCode)) {
+    if (IsAducResultCodeFailure(result.ResultCode))
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         printf("Download Succeeded\n");
         return 0;
     }
@@ -115,7 +143,8 @@ ADUC_Result DownloadRootKeyPkg_DO(const char* url, const char* targetFilePath)
         }
         else
         {
-            fprintf(stderr,
+            fprintf(
+                stderr,
                 "DO error, msg: %s, code: %#08x, timeout? %d\n",
                 ret.message().c_str(),
                 ret.value(),
