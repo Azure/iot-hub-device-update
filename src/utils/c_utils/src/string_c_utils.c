d9a747899313a102e3f1c90eb9c6b0c207b7bc88
jw-msft <84477130+jw-msft@users.noreply.github.com>
2025-04-03 12:48:00 -0700

    fix codeql string_c_utils.c: 430

--- src/utils/c_utils/src/string_c_utils.c
+++ src/utils/c_utils/src/string_c_utils.c
@@ -427,7 +427,7 @@ char* ADUC_StringUtils_Map(const char* src, int (*mapFn)(int))
         return NULL;
     }
 
-    for (int i = 0; i <= len; ++i)
+    for (size_t i = 0; i <= len; ++i)
     {
         int ret = mapFn(src[i]);
 
