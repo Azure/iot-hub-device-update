f36adb1c95cf1480e1acc4f809abf8900c04631a
Andre Ricardo <andre.ricardo@zoetis.com>
2026-02-26 16:47:42 +0000

    Fix do-client logging initialization.

--- src/extensions/content_downloaders/deliveryoptimization_downloader/deliveryoptimization_content_downloader.EXPORTS.cpp
+++ src/extensions/content_downloaders/deliveryoptimization_downloader/deliveryoptimization_content_downloader.EXPORTS.cpp
@@ -54,10 +54,12 @@ EXPORTED_METHOD ADUC_Result GetContractInfo(ADUC_ExtensionContractInfo* contract
  * @brief Initializes the content downloader.
  *
  * @param initializeData The initialization data.
+ * @param logLevel The desired loglevel if logging is used.
  * @return ADUC_Result The result.
  */
-EXPORTED_METHOD ADUC_Result Initialize(const char* initializeData)
+EXPORTED_METHOD ADUC_Result Initialize(const char* initializeData, ADUC_LOG_SEVERITY logLevel)
 {
+    ADUC_Logging_Init(logLevel, "deliveryoptimization-content-downloader");
     ADUC_Result result{ ADUC_GeneralResult_Success };
 
 #if defined(WIN32)
@@ -97,6 +99,14 @@ EXPORTED_METHOD ADUC_Result Initialize(const char* initializeData)
 
 done:
     return result;
+}
+
+/**
+ * @brief Cleanup logic before library is unloaded.
+ */
+EXPORTED_METHOD void Cleanup()
+{
+    ADUC_Logging_Uninit();
 }
 
 /**
