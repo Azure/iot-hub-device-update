f9fadb941c14c799013943b6460d4bb3f5b4ba10
mfsmoot <mirabaismoot@microsoft.com>
2026-02-26 10:06:27 -0800

    Reapply "add run_coverage.sh script to generate code coverage report"
    This reverts commit a15919931c09cd1a154bd62e2534f5de3c07d77b.

--- scripts/build.sh
+++ scripts/build.sh
@@ -49,7 +49,8 @@ install_adu=false
 work_folder="$root_dir/.workspace"
 cmake_dir_path="${work_folder}/deviceupdate-cmake"
 cmake_bin="cmake"
-rootkeypkg_curl=true
+rootkeypkg_curl=false
+enable_coverage=false
 
 #
 # Export the compiler settings in case VM is wonky
@@ -112,6 +113,9 @@ Usage: build.sh [options...]
     --patch-version                       Patch version of ADU
 
     --rootkeypkg-curl                     Download the RootKey Package with curl instead of delivery optimization agent.
+
+    --coverage                            Enable coverage mode in build.sh:
+                                            instrumented build, force unit tests, then run test+report workflow.
 
     -h, --help                            Show this help message.
 
@@ -374,6 +378,9 @@ while [[ $1 != "" ]]; do
     --rootkeypkg-curl)
         rootkeypkg_curl="true"
         ;;
+    --coverage)
+        enable_coverage=true
+        ;;
     -h | --help)
         print_help
         $ret 0
@@ -391,6 +398,16 @@ done
 # work_folder has been set if --work-folder was specified.
 if [[ -z $cmake_dir_path ]]; then
     cmake_dir_path="${work_folder}/deviceupdate-cmake"
+fi
+
+# Coverage mode is owned by build.sh. It forces unit-test build and
+# applies instrumentation flags so the follow-up coverage step has data.
+if [[ $enable_coverage == "true" ]]; then
+    build_unittests=true
+    build_type=Debug
+    export CFLAGS="--coverage -O0 -g ${CFLAGS:-}"
+    export CXXFLAGS="--coverage -O0 -g ${CXXFLAGS:-}"
+    export LDFLAGS="--coverage ${LDFLAGS:-}"
 fi
 
 # Source build environment from install-deps.sh if it exists
@@ -452,6 +469,7 @@ bullet "Enable file log: $file_log"
 bullet "Logging library: $log_lib"
 bullet "Output directory: $output_directory"
 bullet "Build unit tests: $build_unittests"
+bullet "Coverage mode: $enable_coverage"
 bullet "Enable E2E testing: $enable_e2e_testing"
 bullet "Build packages: $build_packages"
 bullet "CMake: $cmake_bin"
@@ -624,6 +642,17 @@ if [[ $ret_val == 0 && $build_packages == "true" ]]; then
 fi
 
 popd > /dev/null || $ret
+
+if [[ $ret_val == 0 && $enable_coverage == "true" ]]; then
+    coverage_script="$script_dir/run_coverage.sh"
+    if [[ ! -x $coverage_script ]]; then
+        error "Coverage script not found or not executable: $coverage_script"
+        $ret 1
+    fi
+
+    "$coverage_script" --out-dir "$output_directory"
+    ret_val=$?
+fi
 
 if [[ $ret_val == 0 && $install_adu == "true" ]]; then
     install_adu_components
