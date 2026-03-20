cf8efb88009ceb7cce312640c63829355ae62d29
yinaliu <yinaliu@microsoft.com>
2026-03-20 23:35:12 +0000

    set rootkeypkg_curl to true

--- scripts/build.sh
+++ scripts/build.sh
@@ -57,7 +57,7 @@ install_adu=false
 work_folder="$root_dir/.workspace"
 cmake_dir_path="${work_folder}/deviceupdate-cmake"
 cmake_bin="cmake"
-rootkeypkg_curl=false
+rootkeypkg_curl=true
 enable_coverage=false
 support_delivery_optimization=false
 
